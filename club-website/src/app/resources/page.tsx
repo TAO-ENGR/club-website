391a83709679a7ce8d32f6d8ab8564e62b614cfd
Sophia Phu <sophiatiffphu@gmail.com>
2024-04-14 15:57:47 -0500

    started resources page

---
+++ club-website/src/app/resources/page.tsx
@@ -0,0 +1,11 @@
+export default function resources() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 gap-12">
+        <div>
+            <p>
+                start of resources pageeeee
+            </p>
+        </div>
+    </main>
+  );
+}
