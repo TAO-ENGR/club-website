3c6d9cf81a4c519ae1cc5e6e8fa220a70fc9e16e
Ashley <ashleyoftexas@gmail.com>
2024-04-15 14:01:03 -0500

    about page finished

--- club-website/src/pages/About/About.jsx
+++ club-website/src/pages/About/About.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function About() {
     return (
-        <div>
-            testing testing
-        </div>
+        <>
+            <div className="text-left bg bg-light-gray p-16 h-screen">
+                <h1 className="Poppins font-bold text-6xl mb-10">ABOUT</h1>
+                <p className="Inter text-4xl">
+                    Howdy, we are the <b>Engineering Teaching Assistant Organization (TAO)</b>!
+                    <br/><br/>
+                    We are an engineering student organization that supports the peer teachers, professors, and students of Texas A&M’s first-year engineering courses (ENGR 102 and 216).
+                    <br/><br/>
+                    This is done via building community on our <u><a href="https://tx.ag/taoserver" target="_blank">discord</a></u> -- giving quick answers on on homework and assignments, virtual office hours, and advice from other students and upperclassmen. We also provide in-person and online exam reviews, using the insight of students who previously took the course and current professors which you can view under <u><Link to="/resources">resources</Link></u>.
+                </p>
+            </div>
+        </>
     )
 }
