71386ad1bfb6916dd1a4e7666d1d4eec644b9c1b
csclaire <122688652+csclaire@users.noreply.github.com>
2024-04-17 10:15:01 -0500

    added button, text, review link, and some formatt

--- club-website/src/app/layout.tsx
+++ club-website/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-slate-400" style={inter.style}>{children}</body>
     </html>
   );
 }
--- club-website/src/app/page.tsx
+++ club-website/src/app/page.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
+// import "./globals.css" 
 
 export default function Resources() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 gap-12">
+    <main className="bg-slate-50 flex min-h-screen flex-col items-center justify-between p-24 gap-12">
+      <h1 className="justify-items-start text-slate-950 font-bold ">RESOURCES</h1>
 
+      {/* <img src=""></img> */}
+      <div className="bg-maroon theme-container">
+        <h1 className="text-slate-50 font-bold">FEATURED</h1>
+        <h1 className="text-slate-50 font-bold">CONTENT</h1>
+
+        <h3 className="text-weirdGray font-bold">SPRING 2023 ENGR 216 FINAL</h3>
+        <h3 className="text-weirdGray font-bold">EXAM MATERIALS</h3>
+        <a href="https://drive.google.com/drive/u/4/folders/1Z0DnRWiczanfWoSsFcsjrseWeGW1YDEE" 
+        className="bg-ashyBlue text-slate-50 rounded-2xl px-4 py-1">view drive materials
+        ㅤ‎ ‎‎ ‎ ‎‎ ‎ ‎‎ 
+        ‎→</a>
+      </div>
     </main>
   );
 }
