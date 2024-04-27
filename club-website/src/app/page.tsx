08996f07686df803f2dcbe8a4ba8ded6a830f323
Larissa Kim <kimLarissa04@gmail.com>
2024-04-26 21:59:10 -0500

    header commit

--- club-website/src/app/page.tsx
+++ club-website/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Header from "@/components/header";
+
 
 export default function Resources() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 gap-12">
-
+      <Header></Header>
+      <h1>resources</h1>
     </main>
   );
 }
