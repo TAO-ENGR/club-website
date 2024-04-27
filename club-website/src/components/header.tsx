61bcea2253994d5f9b1d1364f3707f6c71c0c5f6
Larissa Kim <kimLarissa04@gmail.com>
2024-04-26 21:57:16 -0500

    header edits

--- club-website/src/components/header.tsx
+++ club-website/src/components/header.tsx
@@ -1,2 +1,34 @@
-//comment
-import { RiMailLine, RiYoutubeLine, RiGithubLine, RiDriveLine, RiLinkedinBoxLine, RiInstagramLine, RiDiscordLine } from "react-icons/ri";
+// import {
+//     NavigationMenu,
+//     NavigationMenuContent,
+//     NavigationMenuIndicator,
+//     NavigationMenuItem,
+//     NavigationMenuLink,
+//     NavigationMenuList,
+//     NavigationMenuTrigger,
+//     NavigationMenuViewport,
+//   } from "@/components/ui/navigation-menu"
+
+export default function Header() {
+
+    return (
+        <div className="w-full border-2 border-black flex justify-between bg-pink-500">
+            <div>
+                <p>something here</p>
+            </div>
+            {/* <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <NavigationMenuLink>Link</NavigationMenuLink>
+                    </NavigationMenuContent>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu> */}
+        </div>
+        
+    );
+}
+
+//link TAO logo to resources page.. no home page yet sadge
