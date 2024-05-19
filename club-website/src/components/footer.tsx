3694a322ed0e71dc4729acc03691e14f01ee3c76
Andrew800 <andrewzhengschool@gmail.com>
2024-05-20 02:03:46 +0800

    Changed layout to grid (1 for mobile, 1 for desktop) and made mobile design!
    Issues
    - (All) Email icon is super small for some reason :((
    - (Desktop) Need to remove vertical space between About and Resources
    - (Desktop) Need to fix text spacing of address
    - (Mobile) On "md" devices (like the iPad Air), the Announcements text in the Landing section clips into the email text
    - (Mobile) Should we keep the TAO logo/address above the links, or should we put them below the links? (so we make it links then address on top of logo or links then logo on top of address instead)

--- club-website/src/components/footer.tsx
+++ club-website/src/components/footer.tsx
@@ -34,63 +34,46 @@ const LINKS = [
     },
 ]
 
-// Frontend Details
-//  Colors (Tailwind)
-//      maroon: "#731b1d"
-//      darkMaroon: "#4d0000"
-//      weirdGray: "#272a29"
-//      ashyBlue: "#3c4c68"
-//      lightBlue: "#4d8f9f"
-//  Page Layout
-//      24 pixel padding around whole page with 12 pixel gap between sections
-/*
-        import Image from "next/image";
-
-        export default function Example() {
-        return (
-            <main className="flex min-h-screen flex-col items-center justify-between p-24 gap-12">
-
-            </main>
-        );
-        }
-*/
-//  Fonts
-//      Headers are Poppins XL Bold All-Caps
-//      <h2 className='text-xl font-bold uppercase' style={poppins.style}>Example</h2>
-//      Body text is Inter
-
 export default function Footer()
 {
     return (
-        <main className="flex flex-row items-start justify-between p-24 ps-32 pe-32 gap-12">
-            <div id="logo_address">
-                <Image id="logo" src="/lightLogo.png" alt="" className="w-1/2 h-1/2" width="300" height="300"></Image>
+        <main className="items-start justify-between grid grid-cols-2 md:grid-cols-5 p-8 ps-12 pe-12 md:p-24 md:ps-32 md:pe-32 gap-12">
+            <div className="md:row-span-2 col-span-2" id="logo_address">
+                <img id="logo" src="lightLogo.png"/>
                 <p>
-                    &nbsp;&nbsp;ZACH 420 - 125 Spence St, College<br/>
-                    &nbsp;&nbsp;Station, TX 77843, United States<br/>
+                    ZACH 420 - 125 Spence St, College Station, TX 77843, United States<br/>
                     <br/>
-                    &nbsp;&nbsp;2024 © ENGR TA Organization
+                    2023 © ENGR TA Organization
                 </p>
             </div>
 
-            <div id="about_resources" className='flex flex-col'>
-                <Link href="/about" className="hover:text-maroon font-bold">About</Link>
-                <Link href="/about/#impact" className="hover:text-maroon">Impact</Link>
-                <Link href="/about/#officers" className="hover:text-maroon">Officers</Link>
-                <p>-</p>
-                <Link href="/resources" className="hover:text-maroon font-bold">Resources</Link>
-                {/* <Link href="/">ENGR 102</Link><br/> */}
-                <Link href="/" className="hover:text-maroon">ENGR 216</Link>
-                <Link href="/" className="hover:text-maroon">Miscellaneous</Link>
-                <p>-</p>
+            <div id="about">
+                    <p>
+                    <b><Link href="/about" className="hover:text-maroon">About</Link></b><br/>
+                    <Link href="/about/#impact" className="hover:text-maroon">Impact</Link><br/>
+                    <Link href="/about/#officers" className="hover:text-maroon">Officers</Link><br/>
+                    -
+                    </p>
             </div>
 
-            <div id="landing" className='flex flex-col'>
-                <Link href="/home" className="hover:text-maroon font-bold">Landing</Link>
-                <Link href="/announcements" className="hover:text-maroon">Announcements</Link>
-                <Link href="/announcements" className="hover:text-maroon">Upcoming</Link>
-                <Link href="/calendar" className="hover:text-maroon">Calendar</Link>
-                <p>-</p>
+            <div className="md:row-start-2 md:col-start-3" id="resources">
+                <p>
+                    <b><Link href="/" className="hover:text-maroon">Resources</Link></b><br/>
+                    {/* <Link href="/">ENGR 102</Link><br/> */}
+                    <Link href="/" className="hover:text-maroon">ENGR 216</Link><br/>
+                    <Link href="/" className="hover:text-maroon">Miscellaneous</Link><br/>
+                    -
+                </p>
+            </div>
+
+            <div id="landing">
+                <p>
+                <b><Link href="/home" className="hover:text-maroon">Landing</Link></b><br/>
+                <Link href="/announcements" className="hover:text-maroon">Announcements</Link><br/>
+                <Link href="/announcements/#upcoming" className="hover:text-maroon">Upcoming</Link><br/>
+                <Link href="/calendar" className="hover:text-maroon">Calendar</Link><br/>
+                -
+                </p>
             </div>
 
             <div id="contact" className='flex flex-col'>
@@ -101,6 +84,7 @@ export default function Footer()
                 <a href="https://github.com/TAO-ENGR" target="_blank" className="hover:text-maroon"><div className="flex gap-2 items-center"><RiGithubLine/>TAO-ENGR</div></a>
                 <p>-</p>
             </div>
+
         </main>
     )
 }
