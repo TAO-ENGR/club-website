93fcd25f6e70e79ef636d3b4fb7cea22010fc9dc
tristanaujong <tristanaujong@tamu.edu>
2024-06-08 17:52:28 -0500

    added a lot of styling

--- club-website/src/app/reviews/page.tsx
+++ club-website/src/app/reviews/page.tsx
@@ -2,6 +2,7 @@ import { poppins } from "../fonts";
 import about from "./reviews.module.css";
 import Link from "next/link";
 import Myaccordion from "@/components/ui/Myaccordion";
+import Myaccordion1 from "@/components/ui/Myaccordion1";
 
 export default function Reviews() {
   return (
@@ -10,7 +11,16 @@ export default function Reviews() {
         <h1 className="font-bold text-6xl mb-10" style={poppins.style}>
           REVIEW MATERIALS
         </h1>
-        <Myaccordion/>
+        {/* // flex-row div allows accordions to be side-by-side */}
+        <div className="flex flex-row"> 
+        <div className="basis-1/2 size-100 rounded-lg m-3 bg-gradient-to-br from-red-950 from-20% via-red-800 via-40% to-blue-950 to-90%"> 
+            <Myaccordion/>
+        </div>
+        <div className="basis-1/2 size-100 rounded-lg m-3 bg-gradient-to-br from-red-900 from-10% to-red-400 to-90%"> 
+            <Myaccordion1/>
+        </div>
+        </div>
+        
       </div>
       
     </>
--- club-website/src/components/ui/Myaccordion1.tsx
+++ club-website/src/components/ui/Myaccordion1.tsx
@@ -4,22 +4,22 @@ import {
     AccordionItem,
     AccordionTrigger,
   } from "@/components/ui/accordion"
-  
+
+//  import { poppins } from "../fonts";
 
 // type = "multiple" allows for both accordions to be open
-function Myaccordion() {
+function Myaccordion1() {
     return ( 
         <Accordion type="multiple"> 
 
   <AccordionItem value="item-1"> 
     <AccordionTrigger>
-    <p className="text-2xl font-bold indent-4 text-white">
-        ENGR 102
+        <p className="text-2xl font-bold indent-4 text-white">
+        ENGR 216
         </p>
-      </AccordionTrigger> 
+        </AccordionTrigger> 
     <p className="indent-4 text-white">
-    Exam review materials, practice problems,
-    coding examples, IDE help.
+    Exam review materials, past recordings, lab report materials.
     </p>
     <AccordionContent>
       <AccordionItem value="item-1a">
@@ -29,7 +29,7 @@ function Myaccordion() {
         </p>
         </AccordionTrigger> 
       <AccordionContent>
-        <p className="indent-4 text-white">
+      <p className="indent-4 text-white">
           Final Exam Review
         </p>
       </AccordionContent>
@@ -52,15 +52,17 @@ function Myaccordion() {
       </AccordionContent>
       <AccordionContent>
       <p className="indent-4 text-white">
-      Review Session Recordings
+          Review Session Recordings
         </p>
       </AccordionContent>
       </AccordionItem>
     </AccordionContent>
   </AccordionItem>
+
+  
 </Accordion>
 
     )
 }
 
-export default Myaccordion
+export default Myaccordion1
