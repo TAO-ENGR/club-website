d73774bc7f081a2c1acfb8f52ed42e8eb3c2d442
csclaire <122688652+csclaire@users.noreply.github.com>
2024-06-08 19:01:21 -0500

    changed boxes to take up full page length

--- club-website/src/app/home/page.tsx
+++ club-website/src/app/home/page.tsx
@@ -7,16 +7,16 @@ export default function Home() {
 
             <p className="text-xl pb-7">We have already <s>improved exam and assignment scores</s> fostered a community.</p>
 
-            <div className="flex flex-col lg:flex-row">
-                <div className="bg-maroon flex-initial w-72 border-2 p-6 pb-8 text-white">
+            <div className="flex flex-col md:flex-row">
+                <div className="bg-maroon flex-auto w-72 border-2 p-6 pb-8 text-white">
                     <p className="text-xl mb-2">⬜ Total hours</p>
                     <p className="text-6xl">919.40</p>
                 </div>
-                <div className="bg-gray-100 flex-initial w-72 border-2 p-6 pb-8">
+                <div className="bg-gray-100 flex-auto w-72 border-2 p-6 pb-8">
                     <p className="text-xl mb-2">⬜ Unique members</p>
                     <p className="text-6xl">673</p>
                 </div>
-                <div className="bg-gray-100 flex-inital w-72 border-2 p-6 pb-8">
+                <div className="bg-gray-100 flex-auto w-72 border-2 p-6 pb-8">
                     <p className="text-xl mb-2">⬜ Unique channels</p>
                     <p className="text-6xl">15</p>
                 </div>
