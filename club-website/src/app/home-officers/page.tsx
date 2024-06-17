fdc21ed49df13f26ca9897e61a2681a930546d38
LarissaNamu <kimLarissa04@gmail.com>
2024-06-17 11:23:41 -0500

    added officer json files and pictures

--- club-website/src/app/home-officers/page.tsx
+++ club-website/src/app/home-officers/page.tsx
@@ -1,1 +1,24 @@
-//pr
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel"
+
+export default function Home() {
+    return(
+        <div>
+            <Carousel>
+                <CarouselContent>
+                    <CarouselItem>...</CarouselItem>
+                    <CarouselItem>...</CarouselItem>
+                    <CarouselItem>...</CarouselItem>
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+                </Carousel>
+        </div>
+    )
+
+}
