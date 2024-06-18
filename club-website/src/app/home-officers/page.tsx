0fb4d09670966e2cdc1bfa73152de7cf30eb8882
LarissaNamu <kimLarissa04@gmail.com>
2024-06-18 13:44:23 -0500

    carousel done! - remaining issues -> need to add "see our full officer list" button, missing pictures for some people

--- club-website/src/app/home-officers/page.tsx
+++ club-website/src/app/home-officers/page.tsx
@@ -29,65 +29,122 @@ export default function Home() {
             </h1>
             <Carousel>
                 <CarouselContent>
-                    <CarouselItem className="basis-1/4">
-                        {officers.map((officer, index) => (
+                
+                        {execs.map((exec, index) => (
+                            <CarouselItem className="basis-1/4">
                             <div
-                            className="flex flex-col items-center justify-center text-center w-44"
+                            className="flex flex-col items-center justify-center text-center w-56"
                             key={index}
                             >
-                            {officer.desc == "" ? (
-                                <Image
-                                src={officer.img}
+                            {exec.desc == "" ? (
+                                <img
+                                src={exec.img}
                                 alt=""
-                                width={500}
-                                height={500}
                                 className="w-40 h-40 object-cover mb-4 rounded-2xl"
                                 />
                             ) : (
                                 <HoverCard>
                                 <HoverCardTrigger>
-                                    <Image
-                                    src={officer.img}
+                                    <img
+                                    src={exec.img}
                                     alt=""
-                                    width={500}
-                                    height={500}
                                     className="w-40 h-40 object-cover mb-4 rounded-2xl cursor-pointer"
                                     />
                                 </HoverCardTrigger>
-                                <HoverCardContent>
-                                    <div>{officer.desc}</div>
-                                </HoverCardContent>
+                                <HoverCardContent
+                                    dangerouslySetInnerHTML={{ __html: exec.desc }}
+                                ></HoverCardContent>
                                 </HoverCard>
                             )}
                             <p className={`${home["text-light-gray"]}`}>
-                                {officer.linkedIn == "" ? (
+                                {exec.linkedIn == "" ? (
                                 <b>
-                                    {officer.firstName.toUpperCase()}{" "}
-                                    {officer.lastName.toUpperCase()}
+                                    {exec.firstName.toUpperCase()} {exec.lastName.toUpperCase()}
                                 </b>
                                 ) : (
                                 <a
-                                    href={officer.linkedIn}
+                                    href={exec.linkedIn}
                                     target="_blank"
-                                    className="hover:text-maroon"
+                                    className="hover:text-light-gray"
                                 >
                                     <b>
                                     <u>
-                                        {officer.firstName.toUpperCase()}{" "}
-                                        {officer.lastName.toUpperCase()}
+                                        {exec.firstName.toUpperCase()}{" "}
+                                        {exec.lastName.toUpperCase()}
                                     </u>
                                     </b>
                                 </a>
                                 )}
 
                                 <br />
-                                {officer.position}
+                                {exec.position}
                                 <br />
-                                {officer.major}
+                                {exec.major}
                             </p>
                             </div>
+                            </CarouselItem>
                         ))}
-                    </CarouselItem>
+                        
+                        {officers.map((officer, index) => (
+                            <CarouselItem className="basis-1/4">
+                                <div
+                                className="flex flex-col items-center justify-center text-center w-44"
+                                key={index}
+                                >
+                                {officer.desc == "" ? (
+                                    <Image
+                                    src={officer.img}
+                                    alt=""
+                                    width={500}
+                                    height={500}
+                                    className="w-40 h-40 object-cover mb-4 rounded-2xl"
+                                    />
+                                ) : (
+                                    <HoverCard>
+                                    <HoverCardTrigger>
+                                        <Image
+                                        src={officer.img}
+                                        alt=""
+                                        width={500}
+                                        height={500}
+                                        className="w-40 h-40 object-cover mb-4 rounded-2xl cursor-pointer"
+                                        />
+                                    </HoverCardTrigger>
+                                    <HoverCardContent>
+                                        <div>{officer.desc}</div>
+                                    </HoverCardContent>
+                                    </HoverCard>
+                                )}
+                                <p className={`${home["text-light-gray"]}`}>
+                                    {officer.linkedIn == "" ? (
+                                    <b>
+                                        {officer.firstName.toUpperCase()}{" "}
+                                        {officer.lastName.toUpperCase()}
+                                    </b>
+                                    ) : (
+                                    <a
+                                        href={officer.linkedIn}
+                                        target="_blank"
+                                        className="hover:text-maroon"
+                                    >
+                                        <b>
+                                        <u>
+                                            {officer.firstName.toUpperCase()}{" "}
+                                            {officer.lastName.toUpperCase()}
+                                        </u>
+                                        </b>
+                                    </a>
+                                    )}
+
+                                    <br />
+                                    {officer.position}
+                                    <br />
+                                    {officer.major}
+                                </p>
+                                </div>
+                            </CarouselItem>
+                        ))}
+                    
                 </CarouselContent>
                 <CarouselPrevious />
                 <CarouselNext />
