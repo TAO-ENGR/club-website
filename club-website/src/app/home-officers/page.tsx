322e9b1710ce76326cd81af0dab1149cd6f2dce0
LarissaNamu <kimLarissa04@gmail.com>
2024-06-18 10:51:46 -0500

    hovercards

--- club-website/src/app/home-officers/page.tsx
+++ club-website/src/app/home-officers/page.tsx
@@ -9,21 +9,74 @@ import {
   import Image from "next/image";
   import officerData from "../../../public/officers/all.json";
   import execData from "../../../public/officers/execs.json";
+  import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+  } from "@/components/ui/hover-card"
 
 
 
 export default function Home() {
     return(
         <div>
-            <Carousel>
-                <CarouselContent>
-                    <CarouselItem>...</CarouselItem>
-                    <CarouselItem>...</CarouselItem>
-                    <CarouselItem>...</CarouselItem>
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-                </Carousel>
+            {officers.map((officer, index) => (
+            <div
+              className="flex flex-col items-center justify-center text-center w-44"
+              key={index}
+            >
+              {officer.desc == "" ? (
+                <Image
+                  src={officer.img}
+                  alt=""
+                  width={500}
+                  height={500}
+                  className="w-40 h-40 object-cover mb-4 rounded-2xl"
+                />
+              ) : (
+                <HoverCard>
+                  <HoverCardTrigger>
+                    <Image
+                      src={officer.img}
+                      alt=""
+                      width={500}
+                      height={500}
+                      className="w-40 h-40 object-cover mb-4 rounded-2xl cursor-pointer"
+                    />
+                  </HoverCardTrigger>
+                  <HoverCardContent>
+                    <div>{officer.desc}</div>
+                  </HoverCardContent>
+                </HoverCard>
+              )}
+              <p>
+                {officer.linkedIn == "" ? (
+                  <b>
+                    {officer.firstName.toUpperCase()}{" "}
+                    {officer.lastName.toUpperCase()}
+                  </b>
+                ) : (
+                  <a
+                    href={officer.linkedIn}
+                    target="_blank"
+                    className="hover:text-maroon"
+                  >
+                    <b>
+                      <u>
+                        {officer.firstName.toUpperCase()}{" "}
+                        {officer.lastName.toUpperCase()}
+                      </u>
+                    </b>
+                  </a>
+                )}
+
+                <br />
+                {officer.position}
+                <br />
+                {officer.major}
+              </p>
+            </div>
+          ))}
         </div>
     )
 
