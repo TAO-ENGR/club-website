054b723daae41bd9c5aa87de384ea25a00f06f1d
csclaire <122688652+csclaire@users.noreply.github.com>
2024-06-21 10:00:23 -0500

    made links open in a new tab, changed bg to white, changed img bg to clear

--- club-website/src/app/home/page.tsx
+++ club-website/src/app/home/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <main className="flex flex-col md:flex-row bg-[#f8f4f4] px-24 py-32">
+        <main className="flex flex-col md:flex-row bg-white px-24 py-32">
             <div className="md:pr-32">
                 <h1 className="lg:text-6xl md:text-5xl text-4xl font-extrabold pb-5">WHAT ARE YOU</h1>
                 <h1 className="lg:text-6xl md:text-5xl text-4xl font-extrabold pb-10">WAITING FOR ?!</h1>
@@ -10,12 +10,12 @@ export default function Home() {
                 <h2 className="lg:text-4xl md:text-3xl text-2xl pb-2">Come say hi on our</h2>
                 <h2 className="lg:text-4xl md:text-3xl text-2xl pb-10">discord 👋</h2>
 
-                <p className="lg:text-xl md:text-lg font-light">Don't have a discord? Visit <a href="https://www.discord.com" rel="noopener noreferrer"><u>discord.com</u></a> to</p>
+                <p className="lg:text-xl md:text-lg font-light">Don't have a discord? Visit <a href="https://www.discord.com" target="_blank" rel="noopener noreferrer"><u>discord.com</u></a> to</p>
                 <p className="lg:text-xl md:text-lg font-light">download :&#41;</p>
             </div>
 
             <div className="flex-auto lg:items-center flex justify-center items-center">
-                <a href="https://www.tx.ag/taoserver">
+                <a href="https://www.tx.ag/taoserver" target="_blank" rel="noopener noreferrer">
                     <Image src='/what_are_you_waiting_for.png' flex-initial width={320} height={280} alt="red discord button with cursor"></Image>
                 </a>
             </div>
