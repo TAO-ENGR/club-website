7c3927a32039d56c272569735de4977efa54b856
Ashley <ashleyoftexas@gmail.com>
2024-07-14 15:24:12 -0500

    change to tao channel

--- club-website/src/app/api/announcements/route.ts
+++ club-website/src/app/api/announcements/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-    const res = await fetch(`${process.env.DISCORD_API_ROUTE}/channels/${process.env.TEST_CHANNEL_ID}/messages`, {
+    const res = await fetch(`${process.env.DISCORD_API_ROUTE}/channels/${process.env.TAO_CHANNEL_ID}/messages`, {
         headers: {
             Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`
         }
