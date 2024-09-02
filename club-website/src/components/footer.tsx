bdf5143470a1f4632bbbadc28f16d5ffba3f0f69
Sophia Phu <sophiatiffphu@gmail.com>
2024-09-01 22:48:12 -0500

    fixed footer

--- club-website/src/components/footer.tsx
+++ club-website/src/components/footer.tsx
@@ -43,7 +43,7 @@ const LINKS = [
 
 export default function Footer() {
   return (
-    <footer className="items-start justify-between lg:text-base md:text-md text-sm grid grid-rows-2 md:grid-cols-5 grid-cols-2 gap-8 lg:p-24 lg:ps-32 lg:pe-32 md:p-16 md:ps-16 md:pe-16 p-8 ps-12 pe-12">
+    <footer className="items-start justify-between lg:text-base md:text-md text-sm grid grid-rows-2 md:grid-cols-5 grid-cols-2 gap-8 lg:p-24 lg:ps-24 lg:pe-24 md:p-8 md:ps-8 md:pe-16 p-8 ps-8 pe-8">
       <div
         className="row-start-3 md:row-start-1 col-start-1 col-span-2"
         id="logo"
