6daa41c6354bdcba06b110661c902a0d6907c529
Sophia Phu <sophiatiffphu@gmail.com>
2024-09-27 21:45:04 -0500

    pls work im begging

--- club-website/src/components/announcements.tsx
+++ club-website/src/components/announcements.tsx
@@ -1,45 +1,43 @@
+import { useEffect, useState } from "react";
 import Announcement from "./announcement";
+import axios from "axios";
+
 interface Announcement {
   date: string;
   title: string;
   link: string;
 }
 
-const announcements: Announcement[] = [
-  {
-    date: "8/26",
-    title: "Lab Report Template",
-    link:
-      "https://discord.com/channels/1022962971607060540/1022962972060037222/1277801115731234929"
-  },
-  {
-    date: "8/25",
-    title: "Have an opportunity you'd like to share with TAO?",
-    link:
-      "https://discord.com/channels/1022962971607060540/1022962972060037222/1277409058067841027"
-  },
-  {
-    date: "8/23",
-    title: "Roles for ENGR/PHYS 216 and 217 are available",
-    link:
-      "https://discord.com/channels/1022962971607060540/1022962972060037222/1276563861037649951"
-  },
-  {
-    date: "7/29",
-    title: "Server housekeeping",
-    link:
-      "https://discord.com/channels/1022962971607060540/1022962972060037222/1275577211554103469"
-  }
-];
-
 const Announcements: React.FC = () => {
+  const [announcements, setAnnouncements] = useState<Announcement[]>([]);
+
+  const fetchAnnouncements = async () => {
+    try {
+      const res = await axios.get("/api/announcements");
+
+      if (res.status === 200) {
+        const a: Announcement[] = res.data;
+        setAnnouncements(a);
+      } else {
+        throw new Error(`${res.status} : ${res.statusText}`);
+      }
+    } catch (e: any) {
+      console.log("Error fetching announcements:", e.message);
+      throw e;
+    }
+  };
+
+  useEffect(() => {
+    fetchAnnouncements();
+  }, []);
+
   return (
     <div className="flex flex-col gap-4">
       {announcements.map((announcement, index) => (
         <Announcement
           key={index}
           date={announcement.date}
-          title={announcement.title}
+          title={announcement.title.slice(0, 200) + "..."}
           link={announcement.link}
         />
       ))}
--- club-website/tailwind.config.ts
+++ club-website/tailwind.config.ts
@@ -11,7 +11,7 @@ const config = {
   prefix: "",
   theme: {
   	container: {
-  		center: 'true',
+  		center: true,
   		padding: '2rem',
   		screens: {
   			'2xl': '1400px'
@@ -116,29 +116,12 @@ const config = {
   					opacity: '100%'
   				}
   			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
+
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
   			'float-up': 'float-up 0.75s ease-out',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
